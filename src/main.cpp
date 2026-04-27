160ca01ac2549b853bcd18ea3deaa29c8ac9cfd4
Daniel <d8ikmetov@yandex.ru>
2026-04-27 10:57:22 +0500

    now classes work

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,25 @@ import std;
 class Fraction {
   int _a,_b;//a is numerator //b is denominator
 
-  Fraction(){
-    _a=0;_b=1;
-  }
-  Fraction(int k){
-    _a=k;_b=1;
-  }
-  Fraction(int k,int j){
-    _a=k;
-    _b=(j==0)?1:j;
+  bool check_den(int k)const{
+    if(k!=0) return 0;
+    std::cerr<<"\n"<<"\033[31mError: Denominator cannot be zero!\033[0m"<<std::endl;
+    return 1;
   }
 
   int rdc(){
-    int g=gcd(_a,_b);_a=a/g;_b=b/g;
+    int g=std::gcd(_a,_b);_a=_a/g;_b=_b/g;
     return 0;
   }
 
-  bool check_den(int k)const{
-    if(_k!=0){
-      return 0;
-    }
-    else{
-      std::cerr<<"\n"<<"\033[31mError: Denominator cannot be zero!\033[0m"<<std::endl;
-      return 1;
-    }
-  }
-
-  Fraction(const Fraction&) = default;
-
   public:
-  int num()const return _a
-  int den()const return _b
+  Fraction(const Fraction&) = default;
+  Fraction() : _a(0), _b(1) {}
+  Fraction(int k) : _a(k), _b(1) {}
+  Fraction(int k, int j) : _a(k), _b((j == 0) ? 1 : j) {rdc();}
+
+  int num()const {return _a;}
+  int den()const {return _b;}
 
   int set_num(int k){_a=k;return rdc();}
   int set_den(int k){
@@ -45,24 +33,23 @@ class Fraction {
   }
 
   Fraction& operator+=(const Fraction& other) {
-    checkden(x.num());checkden(y.den());
-    int _a = _a * d.den() + c.num() * _b;
-    int _b = _b * d.den();
+    _a = _a * other._b + other._a * _b;
+    _b = _b * other._b;
     rdc();
     return *this;
   }
-}
+};
 
 Fraction operator+(Fraction a, const Fraction& b) {
   return a += b;
 }
 
-int gcd(int a,int b){//Greatest Common Divisor
-  a = std::abs(a);
-  b = std::abs(b);
-  if (b==0){return a;}//a is numerator //b is denominator
-  else return gcd(b,a%b);
-}
+//int gcd(int a,int b){//Greatest Common Divisor
+//  a = std::abs(a);
+//  b = std::abs(b);
+//  if (b==0){return a;}//a is numerator //b is denominator
+//  else return gcd(b,a%b);
+//}
 
 int promt_user_set(Fraction& x){
   int n,d;
@@ -75,9 +62,9 @@ int promt_user_set(Fraction& x){
 }
 
 int main() {
-  Fraction a;Fraction b;//promt_user_set(a);promt_user_set(b);
+  Fraction a;Fraction b;promt_user_set(a);promt_user_set(b);
 
-//  Fraction result = a+b;
-//  std::cout << "\033[1;32mResult\033[0m:" << result.num() << "/" << result.den() << "\n";
+  Fraction result = a+b;
+  std::cout << "\033[1;32mResult\033[0m:" << result.num() << "/" << result.den() << "\n";
   return 0;
 }
