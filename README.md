2d5adb4298258ef39ab20f8932b114d1b446a91f
Daniel <d8ikmetov@yandex.ru>
2026-04-27 10:28:59 +0500

    Some problems needs to be fixed

--- README.md
+++ README.md
@@ -3,17 +3,3 @@
 cmake -G Ninja -B build
 
 cmake --build build
-
-https://itsecd.ru/oop/lectures/06-operators/
-
-https://itsecd.ru/oop/practices/02-basics/
-
-https://itsecd.ru/oop/practices/04-classes/
-
-https://sourcecraft.dev/itsecd/oop-2026/browse/6111/practice-04/teams/multiplication/iteration3.cpp?rev=main&range=l17c21l17c21
-
-https://github.com/DanielWhy7/cpplabs/blob/lab1/README.md?plain=1
-
-https://itsecd.ru/oop/lectures/oop-05-oop.pdf
-
-https://itsecd.ru/oop/lectures/oop-06-operator-overloading.pdf
--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,23 @@ class Fraction {
   int _a,_b;//a is numerator //b is denominator
 
   Fraction(){
-    _a=0:_b=1;
+    _a=0;_b=1;
   }
-
   Fraction(int k){
     _a=k;_b=1;
   }
-
   Fraction(int k,int j){
-    _a=k;_b=j;
+    _a=k;
+    _b=(j==0)?1:j;
   }
-  public:
-  int num()return _a
-  int den()return _b
-  
-  int set_num(int k){_a=k;return 0};
-  int set_den(int k){
-    if(k!=0){
-      _b=k;
+
+  int rdc(){
+    int g=gcd(_a,_b);_a=a/g;_b=b/g;
+    return 0;
+  }
+
+  bool check_den(int k)const{
+    if(_k!=0){
       return 0;
     }
     else{
@@ -29,45 +28,56 @@ class Fraction {
       return 1;
     }
   }
-  int rdc(){
-    int g=gcd(_a,_b);return {_a/g,_b/g};
+
+  Fraction(const Fraction&) = default;
+
+  public:
+  int num()const return _a
+  int den()const return _b
+
+  int set_num(int k){_a=k;return rdc();}
+  int set_den(int k){
+    if(check_den(k)){
+      _b=k;
+      return 0;
+    }
+    return 1;
   }
-};
+
+  Fraction& operator+=(const Fraction& other) {
+    checkden(x.num());checkden(y.den());
+    int _a = _a * d.den() + c.num() * _b;
+    int _b = _b * d.den();
+    rdc();
+    return *this;
+  }
+}
+
+Fraction operator+(Fraction a, const Fraction& b) {
+  return a += b;
+}
 
 int gcd(int a,int b){//Greatest Common Divisor
   a = std::abs(a);
   b = std::abs(b);
   if (b==0){return a;}//a is numerator //b is denominator
-else return gcd(b,a%b);
+  else return gcd(b,a%b);
 }
 
-Fraction rdc(int a,int b){
-  int g=gcd(a,b);return {a/g,b/g};
-}
-
-Fraction add(int a, int b, int c, int d) {
-  if (b == 0 || d == 0) {
-    std::cerr<<"\n"<<"\033[31mError: Denominator cannot be zero!\033[0m"<<std::endl;
-    return {0, 1};
-  }
-  int num = a * d + c * b;
-  int den = b * d;
-  int g = gcd(num, den);
-  return {num / g, den / g};
+int promt_user_set(Fraction& x){
+  int n,d;
+  std::cout << "Enter a numerator of the first number: ";
+  std::cin >> n;
+  std::cout << "Enter a denominator of the first number: ";
+  std::cin >> d;
+  x=Fraction(n,d);
+  return 0;
 }
 
 int main() {
-  int a = 0, b = 1, c = 0, d = 1;
-  std::cout << "Enter a numerator of the first number: ";
-  std::cin >> a;
-  std::cout << "Enter a denominator of the first number: ";
-  std::cin >> b;
-  std::cout << "Enter a numerator of the second number: ";
-  std::cin >> c;
-  std::cout << "Enter a demominator of the second number: ";
-  std::cin >> d;
+  Fraction a;Fraction b;//promt_user_set(a);promt_user_set(b);
 
-  Fraction result = add(a, b, c, d);
-  std::print("\033[1;32mResult\033[0m: {}/{}\n", result.numerator, result.denominator);
+//  Fraction result = a+b;
+//  std::cout << "\033[1;32mResult\033[0m:" << result.num() << "/" << result.den() << "\n";
   return 0;
 }
